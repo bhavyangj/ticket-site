4211838719bc219b179bce190e9c6be0c40125a0
sciretta <sciretta28@gmail.com>
2023-02-17 09:18:50 -0400

    svgs

---
+++ src/shared/index.ts
@@ -0,0 +1,8 @@
+export const staticFiles = {
+  icons: {
+    cart: "/assets/icons/cart.svg",
+    lock: "/assets/icons/lock.svg",
+    profile: "/assets/icons/profile.svg",
+    sign_up: "/assets/icons/sign_up.svg",
+  },
+};
