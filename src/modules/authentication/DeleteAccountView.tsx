318c2df709c76b6d11b18215b2d757935729f6a3
sciretta <sciretta28@gmail.com>
2023-02-21 22:29:22 -0400

    fix delete account width

--- src/modules/authentication/DeleteAccountView.tsx
+++ src/modules/authentication/DeleteAccountView.tsx
@@ -5,18 +5,18 @@ export const DeleteAccountView: React.FC = () => (
   <div className="min-h-[600px] flex flex-col items-center pt-[5vh] pb-[20vh]">
     <SpaceY />
     <SpaceY />
-    <span className="font-poppins text-red text-sm w-[70vw] text-center">
+    <span className="font-poppins text-red text-sm w-[70vw] max-w-[650px] text-center">
       Do you want to delete your account from our database? If Yes, your data
       will be deleted permanently.
     </span>
     <SpaceY />
     <SpaceY />
-    <span className="font-poppins text-red text-sm w-[70vw] text-center">
+    <span className="font-poppins text-red text-sm w-[70vw] max-w-[650px] text-center">
       Would you like to proceed?
     </span>
     <SpaceY />
     <SpaceY />
-    <div className="flex justify-between w-[70vw]">
+    <div className="flex justify-between w-[70vw] max-w-[650px]">
       <MainButton onClick={() => {}} text="No" />{" "}
       <SecondaryButton onClick={() => {}} text="Yes" />
     </div>
