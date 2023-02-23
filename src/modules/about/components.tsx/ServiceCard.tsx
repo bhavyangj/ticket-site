b539e437c4231b1fcc40176818c188a3ad73368f
sciretta <sciretta28@gmail.com>
2023-02-22 23:05:41 -0400

    fix about us cards width

--- src/modules/about/components.tsx/ServiceCard.tsx
+++ src/modules/about/components.tsx/ServiceCard.tsx
@@ -11,7 +11,7 @@ export const ServiceCard: React.FC<ServiceCardProps> = ({
   icon,
   title,
 }) => (
-  <div className="flex flex-col items-center text-poppins rounded-b-xl px-5 py-14 max-w-[350px] min-w-[250px] drop-shadow-xl bg-white mx-5">
+  <div className="flex flex-col items-center text-poppins rounded-b-xl px-5 py-14 w-[350px] min-w-[250px] drop-shadow-xl bg-white mx-5">
     <img width="45" src={icon} alt="service icon" />
     <SpaceY />
     <span className="font-semibold text-center">{title}</span>
