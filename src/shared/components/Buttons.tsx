cee178a39263b06b5fc14f88819485aabefe554a
sciretta <sciretta28@gmail.com>
2023-02-23 22:45:05 -0400

    container class to main button

--- src/shared/components/Buttons.tsx
+++ src/shared/components/Buttons.tsx
@@ -23,11 +23,10 @@ export const MainButton: React.FC<{
   text: string;
   onClick: () => void;
   disabled?: boolean;
-}> = ({ text, onClick, disabled }) => {
-  const className =
-    "font-poppins font-medium text-white bg-blue w-[300px] py-2";
-  const disabledClass =
-    "font-poppins font-medium text-white bg-blue w-[300px] py-2 cursor-not-allowed";
+  containerClassName?: string;
+}> = ({ text, onClick, disabled, containerClassName = "w-[300px]" }) => {
+  const className = `font-poppins font-medium text-white bg-blue py-2 ${containerClassName}`;
+  const disabledClass = `font-poppins font-medium text-white bg-blue py-2 cursor-not-allowed ${containerClassName}`;
   return (
     <button
       className={disabled ? disabledClass : className}
