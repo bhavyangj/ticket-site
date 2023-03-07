976b3efb5bcf24118a32b19a49046255192f7b9a
sciretta <sciretta28@gmail.com>
2023-03-07 09:55:45 -0400

    show cards

--- src/modules/musicals_and_shows/MusicalsAndShowsView.tsx
+++ src/modules/musicals_and_shows/MusicalsAndShowsView.tsx
@@ -1,70 +1,54 @@
-import { ShowCard, ShowCardProps } from "../../shared/components/ShowCard";
+import { ShowCard, ShowCardProps } from "./components/ShowCard";
 
 const fakeCards: ShowCardProps[] = [
   {
-    name: "엠파이어 스테이트 빌딩 전망대",
-    availabilityStart: "Jan 16",
-    availabilityEnd: "Dec 16",
+    title: "The Lion King",
+    subTitle: "[라이온 킹]",
+    availability: "Sale through Aug 13",
 
-    adultPrice: 48,
-    adultSitePrice: 60,
-    childPrice: 41,
-    childSitePrice: 55,
-    childNote: "만 4 세-12 세 기준, 만 3 세 이하 무료",
+    description:
+      "뉴욕의 명소 브로드웨이. - 연일 매진행렬을 이어오는 가장 인기 있는 뮤지컬",
     image: "/fake/show1.png",
-    isPremium: false,
+    priceStart: 115,
   },
   {
-    name: "탑 오브 더 락 전망대",
-    availabilityStart: "Jan 16",
-    availabilityEnd: "Dec 16",
+    title: "Chicago",
+    subTitle: "[시카고]",
+    availability: "Sale through Aug 13",
 
-    adultPrice: 48,
-    adultSitePrice: 60,
-    childPrice: 41,
-    childSitePrice: 55,
-    childNote: "만 4 세-12 세 기준, 만 3 세 이하 무료",
+    description:
+      "뉴욕의 명소 브로드웨이. - 연일 매진행렬을 이어오는 가장 인기 있는 뮤지컬",
     image: "/fake/show2.png",
-    isPremium: false,
+    priceStart: 59,
   },
   {
-    name: "자유의 여신상 스카이라인 데이크루즈",
-    availabilityStart: "Jan 16",
-    availabilityEnd: "Dec 16",
+    title: "Moulin Rouge! The Musical",
+    subTitle: "[물랑루즈]",
+    availability: "Sale through Aug 13",
 
-    adultPrice: 48,
-    adultSitePrice: 60,
-    childPrice: 41,
-    childSitePrice: 55,
-    childNote: "만 4 세-12 세 기준, 만 3 세 이하 무료",
+    description:
+      "뉴욕의 명소 브로드웨이. - 연일 매진행렬을 이어오는 가장 인기 있는 뮤지컬",
     image: "/fake/show3.png",
-    isPremium: false,
+    priceStart: 119,
   },
   {
-    name: "모마 현대 미술관",
-    availabilityStart: "Jan 16",
-    availabilityEnd: "Dec 16",
-
-    adultPrice: 48,
-    adultSitePrice: 60,
-    childPrice: 41,
-    childSitePrice: 55,
-    childNote: "만 4 세-12 세 기준, 만 3 세 이하 무료",
+    title: "The Book Of Mormon ",
+    subTitle: "[몰몬의 책]",
+    availability: "Sale through Aug 13",
+    description:
+      "뉴욕의 명소 브로드웨이. - 연일 매진행렬을 이어오는 가장 인기 있는 뮤지컬",
     image: "/fake/show4.png",
-    isPremium: false,
+    priceStart: 166.5,
   },
-  {
-    name: "모마 도슨트 투어",
-    availabilityStart: "Jan 16",
-    availabilityEnd: "Dec 16",
 
-    adultPrice: 48,
-    adultSitePrice: 60,
-    childPrice: 41,
-    childSitePrice: 55,
-    childNote: "만 4 세-12 세 기준, 만 3 세 이하 무료",
+  {
+    title: "Aladdin",
+    subTitle: "[알라딘]",
+    availability: "Sale through Aug 13",
+    description:
+      "뉴욕의 명소 브로드웨이. - 연일 매진행렬을 이어오는 가장 인기 있는 뮤지컬",
     image: "/fake/show5.png",
-    isPremium: false,
+    priceStart: 187.5,
   },
 ];
 
@@ -73,7 +57,7 @@ export const MusicalsAndShowsView = () => {
     <div className="flex w-full gap-x-3 justify-center">
       <div className="flex flex-col gap-y-4 w-full max-w-[700px]">
         {fakeCards.map((item) => (
-          <ShowCard key={item.name} {...item} />
+          <ShowCard key={item.title} {...item} />
         ))}
       </div>
     </div>
