c089840e7ffdd6ff6666b9937e93b37cc9a50af8
sciretta <sciretta28@gmail.com>
2023-03-17 20:17:33 -0400

    responsive cover

--- src/shared/components/Cover.tsx
+++ src/shared/components/Cover.tsx
@@ -5,7 +5,7 @@ export const Cover = () => (
     <img className="" src={staticFiles.images.cover_bg} />
     <div className="flex justify-center relative">
       <div className="absolute top-[-180px] lg:top-[-18vw] h-[180px] w-full z-10 flex items-center px-[5vw] max-w-[1300px]">
-        <div className="flex flex-col">
+        <div className="hidden lg:flex flex-col">
           <span className="font-poppins text-white text-xl md:text-3xl font-semibold">
             Big Apple Pass
           </span>
